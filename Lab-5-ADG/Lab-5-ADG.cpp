dc24fe1150b4cfe2672c60a1b8679d3813b3a48b
Gerald656 <gemunoz.seguros@gmail.com>
2025-10-29 22:43:12 -0600

    Termine el triangulo y le hice un pequeño refactor.

--- Lab-5-ADG/Lab-5-ADG.cpp
+++ Lab-5-ADG/Lab-5-ADG.cpp
@@ -5,19 +5,23 @@
 #include "Shape.h"
 #include "Square.h"
 #include "Circle.h"
+#include "Triangle.h"
 
 int main()
 {
 
 	Shape* soyUnaFigura = new Square(2.0);
 	Shape* soyOtraFigura = new Circle(2.0);
+	Shape* soyNuevaFigura = new Triangle(2.0, 2.0);
 
 	double resultadoCuadro = soyUnaFigura->calculateArea();
 	double resultadoCirculo = soyOtraFigura->calculateArea();
-
+	double resultadoTriangulo = soyNuevaFigura->calculateArea();
     std::cout << "Hola, el area de un cuadrado de base 2 es !\n";
 	std::cout << resultadoCuadro;
-	std::cout << "El area de unh circulo con radio 2 es !\n";
-		std::cout << resultadoCirculo;
+	std::cout << "El area de un circulo con radio 2 es !\n";
+	std::cout << resultadoCirculo;
+	std::cout << "El area de un trangulo con base 2 y altura 2 es !\n";
+	std::cout << resultadoTriangulo;
 }
 
